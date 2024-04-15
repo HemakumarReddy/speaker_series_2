9e7d6be534f8f39f137de601ed14d0e34d2cda5f
HemakumarReddy <49957707+HemakumarReddy@users.noreply.github.com>
2024-04-15 23:17:48 +0530

    Delete MicrosoftReactor directory

--- MicrosoftReactor/S7_S8_2021Dec08_15_HandsOnAzureFunctions/Documentation/StarterFiles/MiniProject3/2ReadFromAQS/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, myQueueItem) {
-    context.log('JavaScript queue trigger function processed work item', myQueueItem);
-
-    context.bindings.outputBlob = context.bindings.myQueueItem;
-    context.done();
-
-};
